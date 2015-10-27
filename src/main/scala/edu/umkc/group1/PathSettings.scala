335e44abd37048718993de0b83ad84b34fc44056
jdlars <jlarson.nwciowa@gmail.com>
2015-10-26 21:43:53 -0500

    Uploading Lab 9 for CS5543

---
+++ src/main/scala/edu/umkc/group1/PathSettings.scala
@@ -0,0 +1,15 @@
+package edu.umkc.group1
+
+/**
+ * Created by pradyumnad on 19/07/15.
+ * Modified by Jordan Larson on 10/26/2015
+ */
+object PathSettings {
+
+  val INPUT_DIR = "files/Train"
+
+  val FEATURES_PATH = "modelFace/features"
+  val KMEANS_PATH = "modelFace/clusters"
+  val HISTOGRAM_PATH = "modelFace/histograms"
+  val NAIVE_BAYES_PATH = "modelFace/nbmodel"
+}
